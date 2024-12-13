497859a2378ba61837862920509b5797ff78e78f
Prabhat Kumar <prabhatkumar.cse@gmail.com>
2024-12-13 18:36:57 +0530

    sdkf

--- controllers/recommendationController.js
+++ controllers/recommendationController.js
@@ -1,20 +1,23 @@
-import { generateRecommendations } from '../recommendationAI.js';
-import { dummyProperties } from '../utils/dummyData.js'; 
+import { generateRecommendations } from "../recommendationAI.js";
+import { dummyProperties } from "../utils/dummyData.js";
 
 export const getRecommendations = async (req, res) => {
   const { maxPrice, preferredLocation, propertyType } = req.body; // User preferences sent in the request body
 
   try {
     const userPreferences = {
-      price: maxPrice, 
-      location: preferredLocation, 
-      type: propertyType, 
+      price: maxPrice,
+      location: preferredLocation,
+      type: propertyType,
     };
 
-    const recommendedProperties = generateRecommendations(userPreferences, dummyProperties);
+    const recommendedProperties = generateRecommendations(
+      userPreferences,
+      dummyProperties
+    );
 
     res.json({ recommendedProperties });
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
 };
